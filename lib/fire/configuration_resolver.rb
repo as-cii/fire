bf9e19c624a1178993e92354278dde40bea884c6
Antonio Scandurra <as_cii@hotmail.it>
2014-03-22 12:20:05 +0100

    Find active processes

--- lib/fire/configuration_resolver.rb
+++ lib/fire/configuration_resolver.rb
@@ -2,6 +2,7 @@ require 'yaml'
 
 module Fire
   DOT_FILE = '.fire'
+  PID_FILE = '.fire.pids'
 
   class ConfigurationNotFound < StandardError; end
 
@@ -10,19 +11,26 @@ module Fire
       @stop_at = options[:stop_at] || '/'
     end
 
-    def active_processes
+    def active_processes(path = PID_FILE)
+      raise ConfigurationNotFound if stop?(path)
+      return active_processes("../#{path}") unless File.exist?(path)
 
+      parse_values(path)
     end
 
     def processes(path = DOT_FILE)
       raise ConfigurationNotFound if stop?(path)
       return processes("../#{path}") unless File.exist?(path)
 
-      contents = File.read(path)
-      YAML.load(contents).values
+      parse_values(path)
     end
 
     private
+
+    def parse_values(path)
+      contents = File.read(path)
+      YAML.load(contents).values
+    end
 
     def stop?(path)
       File.dirname(File.absolute_path path) == @stop_at
--- spec/configuration_resolver_spec.rb
+++ spec/configuration_resolver_spec.rb
@@ -17,10 +17,31 @@ describe Fire::ConfigurationResolver do
   end
 
   it 'stops searching configuration when root is reached' do
-    subject = Fire::ConfigurationResolver.new(stop_at: '/')
+    subject = described_class.new(stop_at: '/')
     File.stub(:dirname).and_return('/')
 
     expect { subject.processes }.to raise_error(Fire::ConfigurationNotFound)
+  end
+
+  it 'searches active processes' do
+    File.stub(:exist?).and_return(true)
+    File.stub(:read).and_return(sample_pids)
+
+    expect(subject.active_processes).to eq([ 1, 2 ])
+  end
+
+  it 'searches active processes recursively' do
+    File.stub(:exist?).and_return(false, false, true)
+    File.stub(:read).with("../../#{Fire::PID_FILE}").and_return(sample_pids)
+
+    expect(subject.active_processes).to eq([ 1, 2 ])
+  end
+
+  it 'stops searching active processes when root is reached' do
+    subject = described_class.new(stop_at: '/')
+    File.stub(:dirname).and_return('/')
+
+    expect { subject.active_processes }.to raise_error(Fire::ConfigurationNotFound)
   end
 
   def sample_configuration
@@ -28,5 +49,12 @@ describe Fire::ConfigurationResolver do
     application1: hello
     application2: world
     config
+  end
+
+  def sample_pids
+    <<-pids
+    application1: 1
+    application2: 2
+    pids
   end
 end
